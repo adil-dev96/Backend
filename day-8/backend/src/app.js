66f821b6a2d82fd5facc296f84a036f2ab0b302c
Adil <adildev.core@gmail.com>
2026-02-04 08:40:38 +0530

    Day-8 done

---
+++ day-8/backend/src/app.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const app = express()
+const noteModel = require('./models/note.model')
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors())
+
+
+app.post('/api/notes',async(req,res)=>{
+    const {title,description} = req.body
+
+  const note= await noteModel.create({
+        title,description
+    })
+
+    res.status(201).json({
+        message:"note created successfully",
+        note
+    })
+})
+
+
+app.get('/api/notes',async(req,res)=>{
+    const note = await noteModel.find()
+
+    res.status(200).json({
+        message:"note fetched successfully",
+        note
+    })
+})
+
+
+app.delete('/api/notes/:id',async (req,res)=>{
+    const id = req.params.id
+
+    await noteModel.findByIdAndDelete(id)   
+
+    res.status(201).json({
+        message:"note deleted successfully"
+    })
+})
+
+
+app.patch('/api/notes/:id', async (req,res)=>{
+    const id = req.params.id
+const {description} = req.body
+    await noteModel.findByIdAndUpdate(id,{description})
+
+    res.status(200).json({
+        message:'notes updated successfully'
+    })
+})
+
+module.exports = app
