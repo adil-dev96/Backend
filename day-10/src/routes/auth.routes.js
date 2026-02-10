fc2ad7b92d0ac05d8306be18e99425dba1711292
Adil <adildev.core@gmail.com>
2026-02-10 10:45:16 +0530

    final-commit

--- day-10/src/routes/auth.routes.js
+++ day-10/src/routes/auth.routes.js
@@ -1,19 +1,42 @@
 const express = require('express')
 const userModel = require('../models/user.model')
-
+const jwt = require('jsonwebtoken')
 const authRouter= express.Router()
+
+
+
+
+
 authRouter.post('/register', async (req,res)=>{
     const{email,password,name} = req.body
 
+const isUserAlreadyExist = await userModel.findOne({email})
 
+if (isUserAlreadyExist){
+    return res.status(409).json({
+        message:'user already exist with this email address'
+    })
+}
 
     const user = await userModel.create({
         email,password,name
     })
 
+
+    const token = jwt.sign(
+        {
+            id: user._id,
+            email: user.email
+        },
+        process.env.JWT_SECRET
+    )
+
+res.cookie('jwt_token', token)
+
     res.status(201).json({
         message:'user registered',
-        user
+        user,
+        token
     })
 })
 
