4f5a283e60ba012470fb5a1ddbaf4ca9dd7cb2ba
Adil <adildev.core@gmail.com>
2026-02-13 22:30:26 +0530

    DAY-12

---
+++ day-12/src/model/user.model.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: {
+        type:String,
+        uniqure: [true, "user already exist "]
+    },
+    password: String
+})
+
+
+const userModel = mongoose.model('user', userSchema)
---
+++ day-12/src/routes/auth.route.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const authRouter = express.Router()
+const userModel = require('../model/user.model')
+
+
+
+
+
+
+module.exports = authRouter
