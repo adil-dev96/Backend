6bd54d5df68424907bdc64a4e3de2c6ce661beee
Adil <adildev.core@gmail.com>
2026-02-17 22:09:29 +0530

    auth-done

--- day-12/src/app.js
+++ day-12/src/app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const authRouter = require('./routes/auth.route')
 const app = express()
+const cookieParser = require('cookie-parser')
 
 app.use(express.json())
-
+app.use(cookieParser())
 app.use('/api/auth', authRouter)
 
 module.exports = app
--- day-12/src/model/user.model.js
+++ day-12/src/model/user.model.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name: String,
-    email: {
-        type:String,
-        uniqure: [true, "user already exist "]
-    },
-    password: String
-})
+  name: String,
+  email: {
+    type: String,
+    unique: [true, "user already exist "],
+  },
+  password: String,
+});
 
+const userModel = mongoose.model("user", userSchema);
 
-const userModel = mongoose.model('user', userSchema)
+module.exports = userModel;
