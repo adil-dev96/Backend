28e5f1f3a397c4d4675028899ebef571084efa16
Adil <adildev.core@gmail.com>
2026-02-19 13:26:28 +0530

    login & register

---
+++ day-13/src/controllers/auth.controller.js
@@ -0,0 +1,106 @@
+const userModel = require("../model/user.model");
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+
+
+
+
+async function registerController(req, res) {
+  const { email, username, password, bio, profileImage } = req.body;
+
+  const isUserAlreadyExist = await userModel.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (isUserAlreadyExist) {
+    return res.status(409).json({
+      message:
+        "user already exist " +
+        (isUserAlreadyExist.email == email
+          ? "email already exist"
+          : "username already exist"),
+    });
+  }
+
+  const hash = crypto.createHash("sha256").update(password).digest("hex");
+
+  const user = await userModel.create({
+    username,
+    bio,
+    profileImage,
+    email,
+    password: hash,
+  });
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "1d" },
+  );
+
+  res.cookie("token", token);
+  res.status(201).json({
+    message: "user registered successfully",
+    user: {
+      email: user.email,
+      username: user.username,
+      bio: user.bio,
+      profileImage: user.profileImage,
+    },
+  });
+}
+
+async function loginController(req, res) {
+  const { username, email, password } = req.body;
+
+  const user = await userModel.findOne({
+    $or: [
+      {
+        username: username,
+      },
+      {
+        email: email,
+      },
+    ],
+  });
+
+  if (!user) {
+    return res.status(404).json({
+      message: "user not found",
+    });
+  }
+
+  const hash = crypto.createHash("sha256").update(password).digest("hex");
+
+  const ispasswordvalid = hash == user.password;
+
+  if (!ispasswordvalid) {
+    return res.status(401).json({
+      message: "password invalid",
+    });
+  }
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+
+  res.cookie("token", token);
+
+  res.status(200).json({
+    message: "user logged in successfully",
+    user: {
+      username: user.username,
+      email: user.email,
+      bio: user.bio,
+      profileImage: user.profileImage,
+    },
+  });
+}
+
+
+module.exports={
+    registerController,
+    loginController
+}
---
+++ day-13/src/model/user.model.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: [true, "username already exist"],
+    required: [true, "username is required"],
+  },
+  email: {
+    type: String,
+    unique: [true, "email already exist"],
+    required: [true, "email is required"],
+  },
+  password: {
+    type: String,
+    required: [true, "password is required"],
+  },
+  bio: {
+    type: String,
+  },
+  profileImage: {
+    type: String,
+    default: "https://ik.imagekit.io/lg0khbxcq/default.avif",
+  },
+});
+
+
+
+
+const userModel = mongoose.model ('users', userSchema)
+
+module.exports = userModel
