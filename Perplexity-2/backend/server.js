7a8adc34925fb456a102eeb9ab9ab3a0c69f464a
Adil <adildev.core@gmail.com>
2026-04-25 16:45:56 +0530

    Implement socket.io for real-time chat functionality in backend and frontend

--- Perplexity-2/backend/server.js
+++ Perplexity-2/backend/server.js
@@ -1,19 +1,20 @@
-import "dotenv/config"
-import app from './src/app.js'
-import connectDB from './src/config/database.js'
-
+import "dotenv/config";
+import app from "./src/app.js";
+import connectDB from "./src/config/database.js";
+import http from "http";
+import { initSocket } from "./src/sockets/server.socket.js";
 
 const PORT = process.env.PORT || 3000;
 
+const httpServer = http.createServer(app);
 
+initSocket(httpServer);
 
-connectDB().catch((err)=>{
-    console.error("MongoDB connection failed:", err);
-    process.exit(1)
-    
-})
+connectDB().catch((err) => {
+  console.error("MongoDB connection failed:", err);
+  process.exit(1);
+});
 
-app.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`);
-    
-})
+httpServer.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
