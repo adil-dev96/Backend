d59299636d1c9b2f102a45f7c76acdc32f3b7c03
Adil <adildev.core@gmail.com>
2026-04-25 16:05:51 +0530

    - Added package.json for project dependencies and scripts - Included Vite logo SVG in public directory - Created main App component with RouterProvider - Set up routing with Login and Register pages - Implemented Login and Register forms with state management - Added Tailwind CSS for styling - Configured Vite with React and Tailwind CSS plugins

---
+++ Perplexity-2/frontend/src/features/auth/hook/useAuth.js
@@ -0,0 +1,52 @@
+import { useDispatch } from "react-redux";
+import { register, login, getMe } from "../service/auth.api";
+import { setUser, setLoading, setError } from "../auth.slice";
+
+export function useAuth() {
+  const dispatch = useDispatch();
+
+  async function handleRegister({ email, password, username }) {
+    try {
+      dispatch(setLoading(true));
+      const data = await register({ email, username, password });
+    } catch (error) {
+      dispatch(
+        setError(error.response?.data?.message || "Registration failed"),
+      );
+    } finally {
+      dispatch(setLoading(false));
+    }
+  }
+
+  async function handleLogin({ email, password }) {
+    try {
+      dispatch(setLoading(true));
+      const data = await login({ email, password });
+      dispatch(setUser(data.user));
+    } catch (error) {
+      dispatch(setError(error.response?.data?.message || "Login Failed"));
+    } finally {
+      dispatch(setLoading(false));
+    }
+  }
+
+  async function handleGetMe() {
+    try {
+      dispatch(setLoading(true));
+      const data = await getMe();
+      dispatch(setUser(data.user));
+    } catch (error) {
+      dispatch(
+        setError(error.response?.data?.message || "Failed to fetch user data"),
+      );
+    } finally {
+      dispatch(setLoading(false));
+    }
+  }
+
+  return{
+    handleRegister,
+    handleGetMe,
+    handleLogin
+  }
+}
