0bde020485fc80355058d66077caa77388b8e419
Adil <adildev.core@gmail.com>
2026-04-28 18:32:21 +0530

    Add chat functionality with AI response generation and chat title creation
    - Integrated @langchain/mistralai for enhanced AI capabilities
    - Implemented chat message handling in chat.controller.js
    - Created routes for chat operations in chats.route.js
    - Updated package.json and package-lock.json for new dependencies

--- Perplexity-2/backend/src/app.js
+++ Perplexity-2/backend/src/app.js
@@ -1,26 +1,29 @@
-import express from 'express'
-import cookieParser from 'cookie-parser'
-import authRouter from './routes/auth.routes.js'
-import morgan from "morgan"
-import cors from 'cors'
+import express from "express";
+import cookieParser from "cookie-parser";
+import authRouter from "./routes/auth.routes.js";
+import morgan from "morgan";
+import cors from "cors";
+import chatRouter from "./routes/chats.route.js";
 
+const app = express();
 
-const app = express()
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(morgan("dev"));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "DELETE", "PUT"],
+  }),
+);
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(cookieParser())
-app.use(morgan('dev'))
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true,
-    methods:["GET","POST","DELETE","PUT"]
-}))
+app.get("/", (req, res) => {
+  res.json({ message: "server is running" });
+});
 
-app.get('/', (req,res)=>{
-    res.json({message:"server is running"})
-})
-
-app.use('/api/auth', authRouter )
+app.use("/api/auth", authRouter);
+app.use("/api/chats", chatRouter);
 
 export default app;
--- Perplexity-2/backend/src/services/ai.service.js
+++ Perplexity-2/backend/src/services/ai.service.js
@@ -1,7 +1,40 @@
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { ChatMistralAI } from "@langchain/mistralai";
+import { HumanMessage, SystemMessage,AIMessage } from "langchain";
 
-const model = new ChatGoogleGenerativeAI({
+const geminiModel = new ChatGoogleGenerativeAI({
   model: "gemini-2.5-flash-lite",
-  apiKey: process.env.GEMINI_API_KEY
+  apiKey: process.env.GEMINI_API_KEY,
 });
 
+const mistralModel = new ChatMistralAI({
+  model: "mistral-small-latest",
+  apiKey: process.env.MISTRAL_API_KEY,
+});
+
+export async function generateResponse(messages) {
+  const response = await geminiModel.invoke(messages.map(msg=>{
+    if(msg.role=="user"){
+      return new HumanMessage(msg.content)
+    }else if(msg.role=="ai"){
+      return new AIMessage(msg.content)
+    }
+  }));
+
+  return response.text;
+}
+
+export async function generateChatTitle(message) {
+  const response = await mistralModel.invoke([
+    new SystemMessage(`you are a helpful assistent that generate consise and descriptive title for chat converstations
+  
+  user will provide you with the first message of the chat conversation and you will generate a title that capture the essence of the conversation in 2-3 words . The title should be clear, relevent , and engaging, giving the user a quick understanding of the chats topic
+  
+  `),
+    new HumanMessage(`Generate a title for the chat conversation based on the follwing first message:
+    ${message}
+    `),
+  ]);
+
+  return response.text;
+}
