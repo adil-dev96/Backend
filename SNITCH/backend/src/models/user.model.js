e469f65f352a73e92e7b8659c6129896b1288959
Adil <adildev.core@gmail.com>
2026-09-03 00:04:45 +0530

    feat: initialize backend with Express, MongoDB, and essential configurations
    - Added package.json for backend dependencies and scripts
    - Created server.js to set up the Express server and connect to MongoDB
    - Implemented app.js for middleware setup and basic route
    - Configured database connection in db.js and config.js
    - Defined user model in user.model.js
    - Added morgan for logging HTTP requests

---
+++ SNITCH/backend/src/models/user.model.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+
+const userSchema = new mongoose.Schema({
+    email:{type:String, required:true, unique:true},
+    contact:{type:String, required:true},
+    password:{type:String, required:true},
+    fullname:{type:String, required:true},
+    role:{
+        type:String,
+        enum:["buyer","seller"],
+        default:'buyer'
+    }
+})
+
+
+const userModel = mongoose.model('user', userSchema);
+
+export default userModel;
