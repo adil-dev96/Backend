61407428a69b2d6eef57fe281c1c43e8d9a9d60c
Adil <adildev.core@gmail.com>
2026-09-05 12:47:08 +0530

    initialize frontend project with React, Vite, Tailwind CSS, and routing configuration

--- SNITCH/Frontend/src/app/App.jsx
+++ SNITCH/Frontend/src/app/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-
 import './App.css'
+import { RouterProvider } from 'react-router'
+import { routes } from "./app.routes"
 
 function App() {
 
 
   return (
-    <><h1>Hlw world </h1> </>
+    <RouterProvider router={routes} />
   )
 }
 
-export default App
+export default App 
