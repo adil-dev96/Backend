da26c90f0335d8fe41ace06a23436449c1e62c9e
Adil <adildev.core@gmail.com>
2026-09-09 23:48:24 +0530

    Implement seller authentication middleware and add product model and routes

---
+++ SNITCH/backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+import {config} from "../config/config.js";
+import userModel from "../models/user.model.js";
+
+
+
+export const authenticateSeller = async(req,res,next)=>{
+    const token = req.cookies.token
+
+    if(!token){
+        return res.status(401).json({message:"unauthorized"})
+    }
+
+    try{
+        const decoded = jwt.verify(token, config.JWT_SECRET)
+
+        const user = await userModel.findById(decoded.id)
+
+        if(!user){
+            return res.status(401).json({message:"unauthorized"})
+        }
+
+        if(user.role !=="seller"){
+            return res.status(403).json({message:"forbidden"})
+        }
+    }
+    
+    catch(err){}
+}
